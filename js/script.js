86c5afb397c04e93f3d59dd152bd8c2c25a5d82f
María <maria.lozano12200@gmail.com>
2025-05-26 18:38:28 +0200

    Añadido botón para cambiar de idioma, página traducida y cv subido

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+if (document.getElementById('my-projects-link')) {
+  document.getElementById('my-projects-link').addEventListener('click', () => {
+    document.getElementById('my-projects-section').scrollIntoView({behavior: "smooth"})
   })
 }
